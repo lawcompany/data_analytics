3663e3b344570c3b4bdfc3fd8b7fdc728a6416d5
Chanjun-kim <nice3803@gmail.com>
2022-08-02 02:47:47 +0000

    Update code and except credential

--- adhoc/cj/notebook/keyword_eda.py
+++ adhoc/cj/notebook/keyword_eda.py
@@ -17,6 +17,9 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt 
 import matplotlib.font_manager as fm  
 
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
 
 from plotnine import *
 
@@ -53,6 +56,24 @@ def bigquery_to_pandas(query_string) :
     )
     
     return b
+
+def gs_append(url, credential_path = '../99_credential/lawtalk-bigquery-2bfd97cf0729.json', scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']) : 
+    
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(credential_path, scope)
+    
+    gc = gspread.authorize(credentials)
+    doc = gc.open_by_url(url)
+    
+    sheet_name = re.search(string = str(doc.get_worksheet(0)), pattern = r"\'(.*)\'").group(0)[1:-1]
+    sheet = doc.worksheet(sheet_name)
+    
+    sheet_content = sheet.get_all_values()
+    
+    df_ = pd.DataFrame(sheet_content[1:], columns = sheet_content[0])
+    
+    return df_
+
+
 
 
 adorder_query = '''
@@ -241,12 +262,27 @@ LEFT JOIN adlocation USING (adorders_id)
 LEFT JOIN adkeyword USING (adorders_id)
 LEFT JOIN adnormal USING (adorders_id)
 )
+, slug_lawyers AS (
+SELECT 
+  _id
+  , slug
+FROM `lawtalk-bigquery.raw.lawyers` AS B
+WHERE 1 = 1
+  AND REGEXP_CONTAINS(slug, r'\d{4}-[가-힣]+')
+)
 , adorder_lawyer AS (
 SELECT 
-   A.*
-   , B.slug
-FROM adorder_end AS A
-LEFT JOIN `lawtalk-bigquery.raw.lawyers` AS B
+  B._id AS lawyers_id
+  , B.slug
+  , A.item_name
+  , A.adorders_start_date	
+  , A.adorders_end_date	
+  , A.pauseHistory_startAt_date	
+  , A.pauseHistory_endAt_date
+  , A.adorders_status
+  , A.adpayments_status
+FROM slug_lawyers AS B
+FULL OUTER JOIN adorder_end AS A
 ON A.lawyers_id = B._id
 )
 , tmp_table AS (
@@ -260,13 +296,13 @@ SELECT
   , pauseHistory_endAt_date
   , adorders_status
   , adpayments_status
-  , CASE WHEN adorders_status = 'apply' AND adpayments_status = 'paid' THEN '광고주' ELSE '비광고주' END AS ad_tf
-  , CASE WHEN adorders_status = 'apply' AND adpayments_status = 'paid' THEN 1 ELSE 0 END AS ad_cnt
+  , CASE WHEN adorders_status = 'apply' AND adpayments_status = 'paid' AND '2022-07-01' BETWEEN adorders_start_date AND adorders_end_date AND ('2022-07-01' NOT BETWEEN pauseHistory_startAt_date AND pauseHistory_endAt_date	OR pauseHistory_startAt_date IS NULL) THEN '광고주' ELSE '비광고주' END AS ad_tf
+  , CASE WHEN adorders_status = 'apply' AND adpayments_status = 'paid' AND '2022-07-01' BETWEEN adorders_start_date AND adorders_end_date AND ('2022-07-01' NOT BETWEEN pauseHistory_startAt_date AND pauseHistory_endAt_date	OR pauseHistory_startAt_date IS NULL) THEN 1 ELSE 0 END AS ad_cnt
 FROM adorder_lawyer
 WHERE 1 = 1
-  AND '2022-07-01' BETWEEN adorders_start_date AND adorders_end_date	
+  -- AND '2022-07-01' BETWEEN adorders_start_date AND adorders_end_date
   -- AND pauseHistory_startAt_date IS NOT NULL
-  AND ('2022-07-01' NOT BETWEEN pauseHistory_startAt_date AND pauseHistory_endAt_date	OR pauseHistory_startAt_date IS NULL)
+  -- AND ('2022-07-01' NOT BETWEEN pauseHistory_startAt_date AND pauseHistory_endAt_date	OR pauseHistory_startAt_date IS NULL)
 )
 SELECT 
   slug
@@ -275,6 +311,7 @@ SELECT
   , SUM(ad_cnt) AS ad_cnt
 FROM tmp_table
 GROUP BY 1, 2
+ORDER BY 3 DESC
 ;
 '''
 
@@ -309,12 +346,62 @@ LEFT JOIN adCategory B
 USING (adCategory)
 '''
 
+
+advicet_query = '''
+WITH advicetransaction_tmp AS (
+SELECT
+  _id,
+  row_number() over(PARTITION BY advice ORDER BY updatedAt DESC) AS no_by_advice,
+  number, 
+  DATETIME(createdAt, 'Asia/Seoul') as createdAt,
+  DATETIME(updatedAt, 'Asia/Seoul') as updatedAt,
+  DATETIME(requestedAt, 'Asia/Seoul') as requestedAt,
+  advice,
+  user,
+  lawyer AS lawyer_id,
+  seller,
+  tid,
+  status,
+  method,
+  originFee,
+  canceled,
+  coupon,
+  metadata,
+  REGEXP_EXTRACT(metadata, r"'page': '([ㄱ-ㅎ가-힣a-zA-Z0-9 !#$%&()*+,-./:;<=>?@[\]^_`{|}~]+)'") as metadata_page,
+  REGEXP_EXTRACT(metadata, r"'source': '([ㄱ-ㅎ가-힣a-zA-Z0-9 !#$%&()*+,-./:;<=>?@[\]^_`{|}~]+)'") as metadata_source,
+  REGEXP_EXTRACT(metadata, r"'element': '([ㄱ-ㅎ가-힣a-zA-Z0-9 !#$%&()*+,-./:;<=>?@[\]^_`{|}~]+)'") as metadata_element,
+  REGEXP_EXTRACT(metadata, r"'position': ([0-9]+),") as metadata_position,
+  REGEXP_EXTRACT(metadata, r"'context': '([ㄱ-ㅎ가-힣a-zA-Z0-9 !#$%&()*+,-./:;<=>?@[\]^_`{|}~]+)'") as metadata_context,
+  REGEXP_EXTRACT(metadata, r"'contextAdditional': '([ㄱ-ㅎ가-힣a-zA-Z0-9 !#$%&()*+,-./:;<=>?@[\]^_`{|}~]+)'") as metadata_contextAdditional,
+  REGEXP_EXTRACT(metadata, r"'extraInfo': '([ㄱ-ㅎ가-힣a-zA-Z0-9 !#$%&()*+,-./:;<=>?@[\]^_`{|}~]+)'") as metadata_extraInfo,
+FROM `lawtalk-bigquery.raw.advicetransactions`
+where 1=1
+  AND DATE(DATETIME(createdAt, 'Asia/Seoul')) BETWEEN '2022-07-01' AND '2022-07-15'
+  AND status = 'paid'
+)
+, slug_lawyers AS (
+SELECT 
+  _id
+  , slug
+FROM `lawtalk-bigquery.raw.lawyers` AS B
+WHERE 1 = 1
+  AND REGEXP_CONTAINS(slug, r'\d{4}-[가-힣]+')
+)
+SELECT 
+    A.*
+    , B.slug
+FROM advicetransaction_tmp AS A
+LEFT JOIN slug_lawyers AS B
+ON A.lawyer_id = B._id
+WHERE 1 = 1
+'''
+
 ga_query = '''
 SELECT 
 	event_date	
 	, event_timestamp
 	, user_pseudo_id
-	, (SELECT param.value.string_value FROM UNNEST(event_params) as param WHERE param.key = 'lawyer_id') as lawyer_id 
+	, (SELECT param.value.string_value FROM UNNEST(event_params) as param WHERE param.key = 'lawyer_id') as slug 
 	, (SELECT param.value.string_value FROM UNNEST(event_params) as param WHERE param.key = 'search_lawyer_노출이유_단어') as expose_reason
 	, (SELECT param.value.string_value FROM UNNEST(event_params) as param WHERE param.key = 'search_keyword') as search_keyword
 	, (SELECT param.value.string_value FROM UNNEST(event_params) as param WHERE param.key = 'ga_session_id') as ga_session_id 
@@ -327,11 +414,30 @@ WHERE 1 = 1
 ;
 '''
 
+gae_query = '''
+SELECT 
+	event_date	
+	, event_timestamp
+	, user_pseudo_id
+	, (SELECT param.value.string_value FROM UNNEST(event_params) as param WHERE param.key = 'keyword') as keyword
+	, (SELECT param.value.string_value FROM UNNEST(event_params) as param WHERE param.key = 'ga_session_id') as ga_session_id 
+	, (SELECT param.value.string_value FROM UNNEST(event_params) as param WHERE param.key = 'page_title') as page_title 
+  , (SELECT param.value.string_value FROM UNNEST(event_params) as param WHERE param.key = 'page_location') as page_location 
+FROM `lawtalk-bigquery.analytics_265523655.events_*`
+WHERE 1 = 1 
+	AND _TABLE_SUFFIX BETWEEN '20220701' AND '20220715'
+	AND event_name = 'tag.상단검색창직접입력'
+'''
 
 adorder = bigquery_to_pandas(adorder_query)
 advice = bigquery_to_pandas(advice_query)
+advicet = bigquery_to_pandas(advicet_query)
 ga = bigquery_to_pandas(ga_query)
+gae = bigquery_to_pandas(gae_query)
+lawyer = bigquery_to_pandas(lawyer_query)
 
+keyword = gs_append(url = 'https://docs.google.com/spreadsheets/d/1n9YKC3L9_d4QRKjlDqbHelxw5nC3Fz8hrgbHahOHhxM/edit#gid=0')
+keyword.columns = ["type1", "type2", "search_keyword"]
 
 def get_gini(df, val_col, color = "red", plot_tf = True) :
     tmp = df.copy()
@@ -354,70 +460,109 @@ def get_gini(df, val_col, color = "red", plot_tf = True) :
     
     return (tri - lorenz) / tri
 
+adorder.ad_tf.value_counts()
+
+ga_ad = pd.merge(ga, adorder[["slug", "ad_tf", "ad_cnt"]], on = "slug", how = "left")
+ga_ad.loc[ga_ad.search_keyword.isin(keyword.search_keyword), "mapping"] = 1
+ga_ad.loc[~ga_ad.search_keyword.isin(keyword.search_keyword), "mapping"] = 0
+
+ga_ad.search_keyword = ga_ad.search_keyword.str.replace(pat = "~2F", repl = "/")
+ga_ad.search_keyword = ga_ad.search_keyword.fillna("")
+ga_ad.space_len = ga_ad.search_keyword.str.split(" |/", regex = True).apply(lambda x : len(x))
+
+if1 = (ga_ad.search_keyword == ga_ad.expose_reason) & (ga_ad.search_keyword.notna()) & (ga_ad.search_keyword != "") &  (~ga_ad.search_keyword.str.contains("/")) & (~ga_ad.search_keyword.isin(lawyer.name))
+
+if1_ = (ga_ad.search_keyword != ga_ad.expose_reason) & (ga_ad.search_keyword.notna()) & (ga_ad.search_keyword != "") &  (~ga_ad.search_keyword.str.contains("/")) & (~ga_ad.search_keyword.isin(lawyer.name))
 
 
-ga.groupby("lawyer_id").user_pseudo_id.nunique().sort_values().tail()
-ga.groupby("lawyer_id").event_date.count().sort_values().tail()
-ga.search_keyword = ga.search_keyword.str.replace(pat = "~2F", repl = "/")
-ga.search_keyword = ga.search_keyword.fillna("")
-ga.space_len = ga.search_keyword.str.split(" |/", regex = True).apply(lambda x : len(x))
-
-ga.search_keyword.nunique()
-
-# 조건1 : 검색어와 검색 결과가 같지 않은(대분야 클릭으로 들어오지 않은) 검색어들
-if1 = (ga.search_keyword != ga.expose_reason) & (ga.search_keyword.notna()) & (ga.search_keyword != "") &  (~ga.search_keyword.str.contains("/"))
+# 조건1 : 검색어와 검색 결과가 같지 않고(대분야 클릭으로 들어오지 않은) 변호사 이름이 아닌 검색어들
+if1 = (ga_ad.search_keyword != ga_ad.expose_reason) & (ga_ad.search_keyword.notna()) & (ga_ad.search_keyword != "") &  (~ga_ad.search_keyword.str.contains("/")) & (~ga_ad.search_keyword.isin(lawyer.name))
 # 조건2 : 조건1 + 한단어(뛰어쓰기가 없도록)
-if2 = (ga.search_keyword != ga.expose_reason) & (ga.search_keyword.notna()) & (ga.search_keyword != "") & (ga.space_len == 1) & (~ga.search_keyword.str.contains("/"))
+if2 = (ga_ad.search_keyword != ga_ad.expose_reason) & (ga_ad.search_keyword.notna()) & (ga_ad.search_keyword != "") & (ga_ad.space_len == 1) & (~ga_ad.search_keyword.str.contains("/")) & (~ga_ad.search_keyword.isin(lawyer.name))
 # 조건3 : 조건1에 100개 이상 검색된 결과
-if3 = (ga.search_keyword != ga.expose_reason) & (ga.search_keyword.notna()) & (ga.search_keyword != "") &  (~ga.search_keyword.str.contains("/"))
+if3 = (ga_ad.search_keyword != ga.expose_reason) & (ga_ad.search_keyword.notna()) & (ga_ad.search_keyword != "") &  (~ga_ad.search_keyword.str.contains("/")) & (~ga_ad.search_keyword.isin(lawyer.name))
 # 조건4 : 조건2에 100개 이상 검색된 결과
-if4 = (ga.search_keyword != ga.expose_reason) & (ga.search_keyword.notna()) & (ga.search_keyword != "") & (ga.space_len == 1) & (~ga.search_keyword.str.contains("/"))
-
-ga[if1].search_keyword.value_counts()
-
-ga[if2].search_keyword.value_counts().reset_index()
-
-ga[if3].search_keyword.value_counts().reset_index().query("search_keyword > 100")
-
-ga[(ga.search_keyword != ga.expose_reason) & (ga.search_keyword.notna()) & (ga.search_keyword != "") & (ga.space_len == 1) & (~ga.search_keyword.str.contains("/"))].search_keyword.value_counts().reset_index().query("search_keyword > 100")
-
-ga[(ga.search_keyword != ga.expose_reason) & (ga.search_keyword.notna()) & (ga.search_keyword != "") &  (~ga.search_keyword.str.contains("/"))].search_keyword.value_counts().reset_index().search_keyword.sum()
-
-ga[(ga.search_keyword != ga.expose_reason) & (ga.search_keyword.notna()) & (ga.search_keyword != "") & (ga.space_len == 1) & (~ga.search_keyword.str.contains("/"))].search_keyword.value_counts().reset_index().search_keyword.sum()
-
-ga[(ga.search_keyword != ga.expose_reason) & (ga.search_keyword.notna()) & (ga.search_keyword != "") &  (~ga.search_keyword.str.contains("/"))].search_keyword.value_counts().reset_index().query("search_keyword > 100").search_keyword.sum()
-
-ga[(ga.search_keyword != ga.expose_reason) & (ga.search_keyword.notna()) & (ga.search_keyword != "") & (ga.space_len == 1) & (~ga.search_keyword.str.contains("/"))].search_keyword.value_counts().reset_index().query("search_keyword > 100").search_keyword.sum()
+if4 = (ga_ad.search_keyword != ga_ad.expose_reason) & (ga_ad.search_keyword.notna()) & (ga_ad.search_keyword != "") & (ga_ad.space_len == 1) & (~ga_ad.search_keyword.str.contains("/")) & (~ga_ad.search_keyword.isin(lawyer.name))
 
 
-(
-    ggplot() +
-    
-)
+ga_ad[if1].groupby(["search_keyword"]).slug.count().sort_values(ascending = False)
+ga_ad[if2].groupby("search_keyword").slug.count().sort_values(ascending = False)
+ga_ad[if3].groupby("search_keyword").slug.count().query("search_keyword > 100").sort_values(ascending = False)
+ga_ad[if4].groupby("search_keyword").slug.count().query("search_keyword > 100").sort_values(ascending = False)
+
+ga_ad[if1].groupby("mapping").user_pseudo_id.nunique()
+
+ga_ad[if1 & (ga_ad.mapping == 1)].groupby("search_keyword").user_pseudo_id.nunique().sort_values(ascending = False)
+ga_ad[if1 & (ga_ad.mapping == 0)].groupby("search_keyword").user_pseudo_id.nunique().sort_values(ascending = False)
 
 
-ga[(ga.search_keyword != ga.expose_reason) & (ga.search_keyword.notna()) & (ga.search_keyword != "")].search_keyword.nunique()
+ga[if1].groupby("search_keyword").slug.nunique().sort_values(ascending = False)
+ga[if1].groupby("search_keyword").slug.count().sort_values(ascending = False)
+
+ga[if1].search_keyword.value_counts().reset_index().search_keyword.sum()
+ga[if2].search_keyword.value_counts().reset_index().search_keyword.sum()
+ga[if3].search_keyword.value_counts().reset_index().query("search_keyword > 100").search_keyword.sum()
+ga[if4].search_keyword.value_counts().reset_index().query("search_keyword > 100").search_keyword.sum()
 
 
-ga[(ga.search_keyword != ga.expose_reason) & (ga.search_keyword.notna()) & (ga.search_keyword != "") & (ga.space_len == 1)].search_keyword.value_counts().reset_index()
 
-ga[(ga.search_keyword != ga.expose_reason) & (ga.search_keyword.notna()) & (ga.search_keyword != "") & (ga.space_len == 1)].search_keyword.value_counts().reset_index().query("search_keyword > 100")
+get_gini(ga[if1].groupby("search_keyword").slug.nunique().reset_index(), "slug")
+get_gini(ga[if1].groupby("search_keyword").slug.count().reset_index(), "slug")
 
+get_gini(ga[if2].groupby("search_keyword").slug.nunique().reset_index(), "slug")
 
-ga[(ga.search_keyword != '') & (ga.search_keyword != ga.expose_reason)].search_keyword.value_counts()
-ga[(ga.search_keyword != ga.expose_reason) & (ga.search_keyword.notna()) & (ga.search_keyword != "") & (ga.space_len == 1)].search_keyword.value_counts().head(20)
-
-ga[(ga.search_keyword != ga.expose_reason) & (ga.search_keyword.notna()) & (ga.search_keyword != "") & (ga.space_len == 1)].search_keyword.value_counts().reset_index().query("search_keyword > 100")
-
-get_gini(ga[(ga.search_keyword != '') & (ga.search_keyword != ga.expose_reason)].search_keyword.value_counts().reset_index(), "search_keyword")
-get_gini(ga[(ga.search_keyword != ga.expose_reason) & (ga.search_keyword.notna()) & (ga.search_keyword != "") & (ga.space_len == 1)].search_keyword.value_counts().reset_index(), "search_keyword")
-
-get_gini(ga[(ga.search_keyword != ga.expose_reason) & (ga.search_keyword.notna()) & (ga.search_keyword != "") & (ga.space_len == 1)].search_keyword.value_counts().reset_index().query("search_keyword > 100"), "search_keyword")
-
-
+get_gini(ga[if2].search_keyword.value_counts().reset_index(), "search_keyword")
+get_gini(ga[if3].search_keyword.value_counts().reset_index().query("search_keyword > 100"), "search_keyword")
+get_gini(ga[if4].search_keyword.value_counts().reset_index().query("search_keyword > 100"), "search_keyword")
 
 get_gini(ga.groupby("lawyer_id").event_date.count().reset_index(), "event_date")
-get_gini(ga.groupby("lawyer_id").user_pseudo_id.nunique().reset_index(), "user_pseudo_id")
+get_gini(ga.groupby("slug").user_pseudo_id.nunique().reset_index(), "user_pseudo_id")
+
+ga[if1].groupby("slug").event_date.count().sort_values(ascending = False)
+
+get_gini(ga[(if1)].groupby("lawyer_id").event_date.count().reset_index(), "event_date")
+get_gini(ga[if2].groupby("lawyer_id").event_date.count().reset_index(), "event_date")
+
+get_gini(ga[(if1)].groupby("slug").event_date.count().reset_index(), "event_date")
+get_gini(ga[if2].groupby("slug").event_date.count().reset_index(), "event_date")
 
 
-"pull test"
+
+
+
+
+''''''
+if1 = (advicet.metadata_contextAdditional != advicet.metadata_extraInfo) & (~advicet.metadata_extraInfo.isin(lawyer.name)) & (~advicet.metadata_extraInfo.fillna("").str.contains("/"))
+
+advicet.metadata_context.value_counts()
+
+
+# 5177 -> 1550 약 30%
+advicet[(advicet.metadata_context == 'category') & (advicet.metadata_extraInfo != advicet.metadata_contextAdditional)].metadata_contextAdditional.value_counts()[:10]
+
+for i in advicet[(advicet.metadata_context == 'category') & (advicet.metadata_extraInfo != advicet.metadata_contextAdditional)].metadata_contextAdditional.value_counts()[:10].index :
+    print(advicet[advicet.metadata_contextAdditional.fillna("").str.contains(i)].shape[0])
+
+'''
+'''
+
+
+get_gini(advicet[advicet.metadata_contextAdditional.fillna("").str.contains('임대차')].groupby("lawyer_id")._id.count().reset_index(), "_id")
+
+advicet[advicet.metadata_contextAdditional.fillna("").str.contains('폭행/협박/상해 일반')]
+advicet[advicet.metadata_contextAdditional.fillna("").str.contains('임대차')]
+
+advicet[if1]
+advicet[if1 & (advicet.metadata_extraInfo.isin(keyword.search_keyword))]
+
+advicet[if1].metadata_extraInfo.value_counts()[:100]
+advicet[advicet.metadata_extraInfo == "통신매체이용음란죄"].metadata_contextAdditional.value_counts()
+
+advice
+
+
+for i in list(advicet[if1].metadata_extraInfo.value_counts()[:10].index) :
+    print(advicet[advicet.metadata_extraInfo == i].groupby("slug")._id.count().sort_values(ascending = False)[:1])    
+    get_gini(advicet[advicet.metadata_extraInfo == i].groupby("slug")._id.count().reset_index(), "_id")
+
+
