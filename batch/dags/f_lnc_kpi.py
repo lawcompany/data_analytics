fa86c16bd85c9bdba7384ddcab14278d886acb83
jungarui-lc <ja.lee@lawncompany.co.kr>
2022-08-19 15:07:01 +0900

    [jungarui] 20220819 f_lnc_kpi.py 스케쥴이 제대로 수행되지 않아 배치일자 조정

--- batch/dags/f_lnc_kpi.py
+++ batch/dags/f_lnc_kpi.py
@@ -12,8 +12,8 @@ KST = pendulum.timezone("Asia/Seoul")
 with DAG(
     dag_id="f_lnc_kpi_w",
     description ="lawncompany kpi for shareholders",
-    start_date = datetime(2022, 8, 1, tzinfo = KST),
-    schedule_interval = '0 9 * * 1', #UTC기준?? KST기준인듯
+    start_date = datetime(2022, 8, 3, tzinfo = KST),
+    schedule_interval = '0 9 * * 1', #UTC기준??
     tags=["jungarui","KPI"],
     default_args={
         "owner": "jungarui"#,
@@ -58,7 +58,7 @@ with DAG(
                 from `raw.users`
                 where lawyer is not null
                 and role = 'lawyer'
-                and date(DATETIME(createdAt, 'Asia/Seoul')) <= '{{ds}}'
+                and date(DATETIME(createdAt, 'Asia/Seoul')) <= '{{next_ds}}'
                 ) a
                 inner join `raw.lawyers` b
                 on a.lawyer = b._id
@@ -79,7 +79,7 @@ with DAG(
               ,regexp_extract_all(pauseHistory, r"'startAt': datetime.datetime\((\d{4}, \d{1,2}, \d{1,2}, \d{1,2}, \d{1,2})") as pauseHistory_startAt
               ,regexp_extract_all(pauseHistory, r"'endAt': datetime.datetime\((\d{4}, \d{1,2}, \d{1,2}, \d{1,2}, \d{1,2})") as pauseHistory_endAt
             from `lawtalk-bigquery.raw.adorders`
-            where date(DATETIME(createdAt, 'Asia/Seoul')) <= '{{ds}}'
+            where date(DATETIME(createdAt, 'Asia/Seoul')) <= '{{next_ds}}'
               and REGEXP_CONTAINS(pauseHistory, 'ObjectId')
         )
         , adorders_pausehistory AS (
@@ -152,17 +152,17 @@ with DAG(
                     select *
                     from adoders_arrange
                     where pause_lawyer_id is null
-                    or date_sub(date('{{ds}}','Asia/Seoul'),interval 1 day) not between pauseHistory_startAt_date and pauseHistory_endAt_date
+                    or date_sub(date('{{next_ds}}','Asia/Seoul'),interval 1 day) not between pauseHistory_startAt_date and pauseHistory_endAt_date
                 ) a
                 inner join adpayments_arrange b
                 on a._id = b.paid_lawyer_orders
                 and a.status = 'apply'
                 and b.status = 'paid'
-                and date_sub(date('{{ds}}','Asia/Seoul'),interval 1 day) between date(a.start_date) and date(a.end_date)
+                and date_sub(date('{{next_ds}}','Asia/Seoul'),interval 1 day) between date(a.start_date) and date(a.end_date)
                 union all
                 select distinct a.lawyer
                 from betaadorders_arrange a
-                where date_sub(date('{{ds}}','Asia/Seoul'),interval 1 day) between date(a.start_date) and date(a.end_date)
+                where date_sub(date('{{next_ds}}','Asia/Seoul'),interval 1 day) between date(a.start_date) and date(a.end_date)
             ) a
         )
         , conversion AS
@@ -171,7 +171,7 @@ with DAG(
            from unnest(generate_array(1,3)) as num
         )
         ## 1,2. 변호사회원수&광고주수
-        select date('{{ds}}','Asia/Seoul') as batch_date
+        select date('{{next_ds}}','Asia/Seoul') as batch_date
              , b.b_week
              , b.week_start_date
              , b.week_end_date
@@ -193,7 +193,7 @@ with DAG(
             left join ad_lawyer c
             on b.lawyer = c.lawyer
             inner join `common.d_calendar` d
-            on date_sub(date('{{ds}}','Asia/Seoul'),interval 1 day) = d.full_date
+            on date_sub(date('{{next_ds}}','Asia/Seoul'),interval 1 day) = d.full_date
             left join `for_shareholder.f_lt_lawyer_withdraw` e
             on d.full_date = e.b_date
             group by 1,2,3
@@ -202,7 +202,7 @@ with DAG(
         group by 1,2,3,4,5,6
         union all
         ## 3. 유료상담예약건수
-        select date('{{ds}}','Asia/Seoul') as batch_date
+        select date('{{next_ds}}','Asia/Seoul') as batch_date
              , x.b_week
              , x.week_start_date
              , x.week_end_date
@@ -212,14 +212,14 @@ with DAG(
         from `common.d_calendar` x
         inner join `raw.advicetransactions` a
         on a.status in ('paid','canceled')
-        and x.full_date between date_sub(date('{{ds}}','Asia/Seoul'),interval 7 day) and date_sub(date('{{ds}}','Asia/Seoul'),interval 1 day)
+        and x.full_date between date_sub(date('{{next_ds}}','Asia/Seoul'),interval 7 day) and date_sub(date('{{next_ds}}','Asia/Seoul'),interval 1 day)
         and FORMAT_TIMESTAMP('%Y%m%d', createdAT, 'Asia/Seoul') = x.b_date
         inner join `raw.advice` b
         on a.advice = b._id
         group by 1,2,3,4,5,6
         union all
         ## 4. 050 콜수
-        select date('{{ds}}','Asia/Seoul') as batch_date
+        select date('{{next_ds}}','Asia/Seoul') as batch_date
              , x.b_week
              , x.week_start_date
              , x.week_end_date
@@ -228,13 +228,13 @@ with DAG(
              , count(distinct a._id) as f_value
         from `common.d_calendar` x
         inner join `raw.callevents` a
-        on x.full_date between date_sub(date('{{ds}}','Asia/Seoul'),interval 7 day) and date_sub(date('{{ds}}','Asia/Seoul'),interval 1 day)
+        on x.full_date between date_sub(date('{{next_ds}}','Asia/Seoul'),interval 7 day) and date_sub(date('{{next_ds}}','Asia/Seoul'),interval 1 day)
         and FORMAT_TIMESTAMP('%Y%m%d', startedAT, 'Asia/Seoul') = x.b_date
         and a.type = 'profile'
         group by 1,2,3,4,5,6
         union all
         ## 5. 로톡비즈 변호사 회원 수
-        select date('{{ds}}','Asia/Seoul') as batch_date
+        select date('{{next_ds}}','Asia/Seoul') as batch_date
              , x.b_week
              , x.week_start_date
              , x.week_end_date
@@ -243,13 +243,13 @@ with DAG(
              , count(distinct a.user_name) as f_value
         from `common.d_calendar` x
         inner join `for_shareholder.s_biz_lawyer` a
-        on x.full_date = date_sub(date('{{ds}}','Asia/Seoul'),interval 1 day)
+        on x.full_date = date_sub(date('{{next_ds}}','Asia/Seoul'),interval 1 day)
         and x.full_date = a.b_date
         and FORMAT_TIMESTAMP('%Y%m%d', createdAt, 'Asia/Seoul') <= x.b_date
         group by 1,2,3,4,5,6
         union all
         ## 6. 빅케이스 회원 수
-        select date('{{ds}}','Asia/Seoul') as batch_date
+        select date('{{next_ds}}','Asia/Seoul') as batch_date
              , b.b_week
              , b.week_start_date
              , b.week_end_date
@@ -259,7 +259,7 @@ with DAG(
         from conversion a
         cross join
         (
-            select date('{{ds}}','Asia/Seoul') as batch_date
+            select date('{{next_ds}}','Asia/Seoul') as batch_date
                  , x.b_week
                  , x.week_start_date
                  , x.week_end_date
@@ -269,7 +269,7 @@ with DAG(
             from `common.d_calendar` x
             inner join
             (
-                select date('{{ds}}','Asia/Seoul') as batch_date
+                select date('{{next_ds}}','Asia/Seoul') as batch_date
                      , sum(lawyer) as lawyer
                      , sum(legal_profession) as legal_profession
                      , group_all_user_cnt-sum(lawyer)-sum(legal_profession) as etc_lawyer
@@ -281,19 +281,19 @@ with DAG(
                          , legal_profession
                          , last_value(all_user_cnt) over(order by b_date ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) as group_all_user_cnt
                      from `for_shareholder.f_bc_lawyer`
-                    where b_date < date('{{ds}}','Asia/Seoul')
+                    where b_date < date('{{next_ds}}','Asia/Seoul')
                 ) a
-                group by date('{{ds}}','Asia/Seoul')
+                group by date('{{next_ds}}','Asia/Seoul')
                        , group_all_user_cnt
             ) a
-            on x.full_date = date_sub(date('{{ds}}','Asia/Seoul'),interval 1 day)
+            on x.full_date = date_sub(date('{{next_ds}}','Asia/Seoul'),interval 1 day)
             group by 1,2,3,4
         ) b
         where a.num <= 3
         group by 1,2,3,4,5,6
         union all
         ## 7. 모든변호사 변호사 회원 수
-        select date('{{ds}}','Asia/Seoul') as batch_date
+        select date('{{next_ds}}','Asia/Seoul') as batch_date
              , x.b_week
              , x.week_start_date
              , x.week_end_date
@@ -302,12 +302,12 @@ with DAG(
              , a.lawyer_cnt as f_value
         from `common.d_calendar` x
         inner join `for_shareholder.f_mb_monica_lawyer` a
-        on x.full_date = date_sub(date('{{ds}}','Asia/Seoul'),interval 1 day)-- 어제날짜
+        on x.full_date = date_sub(date('{{next_ds}}','Asia/Seoul'),interval 1 day)-- 어제날짜
         and x.full_date = a.b_date
         group by 1,2,3,4,5,6,7
         union all
         ## 8. 온라인상담 답변글 수
-        select date('{{ds}}','Asia/Seoul') as batch_date
+        select date('{{next_ds}}','Asia/Seoul') as batch_date
              , x.b_week
              , x.week_start_date
              , x.week_end_date
@@ -316,7 +316,7 @@ with DAG(
              , count(distinct a._id) as f_value
         from `common.d_calendar` x
         inner join `raw.answers` a
-        on x.full_date between date_sub(date('{{ds}}','Asia/Seoul'),interval 7 day) and date_sub(date('{{ds}}','Asia/Seoul'),interval 1 day)
+        on x.full_date between date_sub(date('{{next_ds}}','Asia/Seoul'),interval 7 day) and date_sub(date('{{next_ds}}','Asia/Seoul'),interval 1 day)
         and FORMAT_TIMESTAMP('%Y%m%d', createdAT, 'Asia/Seoul') = x.b_date
         group by 1,2,3,4,5,6
         """
